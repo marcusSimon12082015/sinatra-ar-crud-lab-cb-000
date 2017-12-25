9991f77c4642efd99e7d8752251c40e54b6f5102
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-25 18:08:43 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,7 +37,7 @@ class ApplicationController < Sinatra::Base
   patch '/posts/:id' do
     @post = Post.find(params[:id])
     @post.update(name: params[:name], content: params[:content])
-    #binding.pry
+    binding.pry
     #redirect 'posts/#{@post.id}'
   end
 end
